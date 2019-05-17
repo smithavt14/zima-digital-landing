92592dda4d87b8826513594b3edc9ef06a2dd354
Alex Smith <smithavt14@gmail.com>
2019-05-17 12:24:36 +0800

    Added content to the services and work sections

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -1,6 +1,17 @@
+// DOM variables
+
 const navigation = document.querySelector('.navigation_wrapper')
 const logo = document.querySelector('.navigation_zima-digital')
+const services = document.getElementById('services-tab')
+const work = document.getElementById('work-tab')
+const servicesBanner = document.querySelector('.services_banner')
+const workBanner = document.querySelector('.work_banner')
 
+// client rect variables
+let servicesRect = servicesBanner.getBoundingClientRect()
+let workRect = workBanner.getBoundingClientRect()
+
+// Event Listeners
 window.addEventListener('scroll', function(e) {
   if (window.pageYOffset > window.innerHeight - 160) {
     navigation.classList.add('navigation_wrapper-white')
@@ -11,4 +22,12 @@ window.addEventListener('scroll', function(e) {
 
 logo.addEventListener('click', function(e) {
   window.scroll(0,0)
+})
+
+services.addEventListener('click', function(e) {
+  window.scroll(0, servicesRect.top - 100)
+})
+
+work.addEventListener('click', function(e) {
+  window.scroll(0, workRect.top - 100)
 })
