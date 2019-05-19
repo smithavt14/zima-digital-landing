c712f68a2d86841c3d571e387f16081c66f5b940
Alex Smith <smithavt14@gmail.com>
2019-05-19 16:12:44 +0800

    New favicon and created conditional for contact JS

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,11 +1,12 @@
 const contact = document.querySelector('.contact_wrapper')
 
-window.addEventListener('scroll', function(e) {
-  let rect = contact.getBoundingClientRect()
-
-  if (rect.top <= 120) {
-    contact.classList.add('contact_wrapper-fixed')
-  } else {
-    contact.classList.remove('contact_wrapper-fixed')
-  }
-})
+if (window.innerWidth > 800) {
+	window.addEventListener('scroll', function(e) {
+	  let rect = contact.getBoundingClientRect()
+	  if (rect.top <= 120) {
+	    contact.classList.add('contact_wrapper-fixed')
+	  } else {
+	    contact.classList.remove('contact_wrapper-fixed')
+	  }
+	})
+}
