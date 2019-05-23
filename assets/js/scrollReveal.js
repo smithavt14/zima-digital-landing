0678f83691773337ebe487978dde59dcb7bb9ef6
Alex Smith <smithavt14@gmail.com>
2019-05-23 11:27:06 +0800

    Added scroll reveal animation

---
+++ assets/js/scrollReveal.js
@@ -0,0 +1,14 @@
+var slideUp = {
+    distance: '150%',
+    origin: 'bottom',
+    opacity: null,
+};
+
+ScrollReveal().reveal('#move_panel-1', {distance: '150%', origin: 'bottom', opacity: null, delay: 0, cleanup: false});
+ScrollReveal().reveal('#move_panel-2', {distance: '150%', origin: 'bottom', opacity: null, delay: 500});
+ScrollReveal().reveal('#move_panel-3', {distance: '150%', origin: 'bottom', opacity: null, delay: 1000});
+
+ScrollReveal().reveal('#concord_panel-1', {distance: '150%', origin: 'top', opacity: null, delay: 0});
+ScrollReveal().reveal('#concord_panel-2', {distance: '150%', origin: 'top', opacity: null, delay: 500});
+ScrollReveal().reveal('#concord_panel-3', {distance: '150%', origin: 'top', opacity: null, delay: 1000});
+
